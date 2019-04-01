03b9daefa20b0f2c3263ffc0c206c8f4f96143b4
Shay Bergmann <shay.bergmann@iohk.io>
2019-04-01 18:35:08 -0400

    Example 4: Make build script work!

--- default.nix
+++ default.nix
@@ -2,6 +2,9 @@ builtins.derivation {
   system  = builtins.currentSystem;
   name    = "hello-world";
   builder = "/bin/bash";
+
+  PATH    = "/usr/bin:/bin";
+
   args    = [ (builtins.toFile "my-builder" ''
     cat <<EOF > $out
     #!/usr/bin/env bash
@@ -12,7 +15,14 @@ builtins.derivation {
   '') ];
 }
 
-# This still fails to realize. Why?
-# Because the build environment of a Nix derivation is sanitized. i.e.,
-# the environment is cleared and PATH is empty.
-# Let's fix this...
+# Alright! It's working! YOU SHOULD NEVER EVER DO THIS THOUGH! Why?
+# We are basically using utils provided by the underlying OS.
+# Why is this bad? Because impurity!
+# 1) Those come with no guarantees; I'm on darwin (aka macOS) and apple is
+#    known for occasionally breaking all the things without warning.
+# 2) Sandboxing is currently b0rked on darwin and therefore I can't use it.
+#    Had I been able to use it, I would've been. Had sandboxing been on,
+#    this would've failed miserably since the whole thing would be running
+#    in a chroot where `/{,usr}/bin` are not even available.
+#
+# kk so let's fix this...
