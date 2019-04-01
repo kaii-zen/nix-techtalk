4b55ce4b203a76b2f58ed4467b1b9658a6d28683
Shay Bergmann <shay.bergmann@iohk.io>
2019-04-01 18:52:57 -0400

    Example 5: PURIFY!!!!!11111

--- default.nix
+++ default.nix
@@ -1,28 +1,24 @@
-builtins.derivation {
-  system  = builtins.currentSystem;
-  name    = "hello-world";
-  builder = "/bin/bash";
-
-  PATH    = "/usr/bin:/bin";
-
-  args    = [ (builtins.toFile "my-builder" ''
+let
+  builder = builtins.toFile "my-builder" ''
     cat <<EOF > $out
-    #!/usr/bin/env bash
+    #!$builder
+    PATH=$PATH
     echo "hello world"
     EOF
 
     chmod +x $out
-  '') ];
+  '';
+
+  pkgs = import <nixpkgs> {};
+
+in builtins.derivation {
+  system  = builtins.currentSystem;
+  name    = "hello-world";
+  builder = "${pkgs.bash}/bin/bash";
+  args    = [ builder ];
+
+  PATH    = "${pkgs.coreutils}/bin";
 }
 
-# Alright! It's working! YOU SHOULD NEVER EVER DO THIS THOUGH! Why?
-# We are basically using utils provided by the underlying OS.
-# Why is this bad? Because impurity!
-# 1) Those come with no guarantees; I'm on darwin (aka macOS) and apple is
-#    known for occasionally breaking all the things without warning.
-# 2) Sandboxing is currently b0rked on darwin and therefore I can't use it.
-#    Had I been able to use it, I would've been. Had sandboxing been on,
-#    this would've failed miserably since the whole thing would be running
-#    in a chroot where `/{,usr}/bin` are not even available.
-#
-# kk so let's fix this...
+# Much better! Everything is nice and pure!
+# Time to install our package (hint: it ain't gonna work 🙃 )
