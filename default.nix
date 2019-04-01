dd0cca70cd9bda89d3c755f5bf99cfffbb7f3298
Shay Bergmann <shay.bergmann@iohk.io>
2019-04-01 18:13:50 -0400

    Example 3: add build script

--- default.nix
+++ default.nix
@@ -1,8 +1,18 @@
 builtins.derivation {
   system  = builtins.currentSystem;
   name    = "hello-world";
-  builder = "/bin/echo";
-  args    = [ "hello world" ];
+  builder = "/bin/bash";
+  args    = [ (builtins.toFile "my-builder" ''
+    cat <<EOF > $out
+    #!/usr/bin/env bash
+    echo "hello world"
+    EOF
+
+    chmod +x $out
+  '') ];
 }
 
-# This won't "build". Or in Nix parlance, it would fail to realize. Why?
+# This still fails to realize. Why?
+# Because the build environment of a Nix derivation is sanitized. i.e.,
+# the environment is cleared and PATH is empty.
+# Let's fix this...
