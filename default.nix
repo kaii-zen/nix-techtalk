c0a573dd358328c9471fb519a43c7ba4aea2d922
Shay Bergmann <shay.bergmann@iohk.io>
2019-04-01 22:21:41 -0400

    Example 11: I18n part I. 🇪🇺

--- default.nix
+++ default.nix
@@ -1,9 +1,16 @@
 let
   pkgs = import <nixpkgs> {};
 
-in pkgs.writeShellScriptBin "hello-world" ''
-  PATH=${with pkgs; lib.makeBinPath [ coreutils figlet lolcat ]}
-  echo "hello world" | figlet | lolcat
-''
+  dictionary = with pkgs.lib; mapAttrs' (name: value: (nameValuePair (toLower name) value)) (importJSON ./hello-world.json);
 
-# Whoa! Not so ugly anymore! Let's push the envelope a little further.
+  mkI15dHelloWorld = language: greeting: pkgs.writeShellScriptBin "hello-world-${language}" ''
+    PATH=${with pkgs; lib.makeBinPath [ coreutils figlet lolcat ]}
+    echo "${greeting}" | figlet | lolcat
+  '';
+
+in pkgs.lib.mapAttrs mkI15dHelloWorld dictionary
+
+# Awesome! Looks like we're IOHK ready. Internationalized and ready
+# to go and cascadingly disrupt all the things!
+# Or ARE WE? What about Korean, Chinese, Japanese, Russian?
+# My own language (Hebrew) isn't even in the dictionary 🤨
