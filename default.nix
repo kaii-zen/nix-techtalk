4ad6ad7c693a0450448a1418ca6e56f7742c6e0a
Shay Bergmann <shay.bergmann@iohk.io>
2019-04-01 21:10:42 -0400

    Example 10: Introduce trivial builders 🐷💄

--- default.nix
+++ default.nix
@@ -1,24 +1,9 @@
 let
   pkgs = import <nixpkgs> {};
 
-in pkgs.stdenv.mkDerivation {
-  name = "hello-world";
+in pkgs.writeShellScriptBin "hello-world" ''
+  PATH=${with pkgs; lib.makeBinPath [ coreutils figlet lolcat ]}
+  echo "hello world" | figlet | lolcat
+''
 
-  buildCommand = ''
-    mkdir -p $out/bin
-
-    executable=$_/$name
-
-    cat <<EOF > $executable
-    #!${pkgs.stdenv.shell}
-    PATH=${with pkgs; lib.makeBinPath [ coreutils figlet lolcat ]}
-    echo "hello world" | figlet | lolcat
-    EOF
-
-    chmod +x $executable
-  '';
-}
-
-# So we've touched on closures and and trimmed ours back down.
-# That build command is still a bit... meh.
-# Let's see if we can improve this a bit more.
+# Whoa! Not so ugly anymore! Let's push the envelope a little further.
